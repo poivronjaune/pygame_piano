1ada8e1018da48942308b7c70b23457ed49a1d90
PoivronJaune <poivronjaune@gmail.com>
2025-11-28 15:44:49 -0500

    Refactor TOML Dynamic version

--- piano/__init__.py
+++ piano/__init__.py
@@ -1,1 +1,15 @@
-__version__ = "1.0.0"
+import tomllib
+from pathlib import Path
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("piano")
+except PackageNotFoundError:
+    __version__ = None
+    raise RuntimeError("piano package is not installed properly. Check TOML file and use 'pip install -e .'")
+
+def version():
+    """Display bhtp version information."""
+    print(f"bhtp version: {__version__}")
+
+__all__ = ["__version__"]
